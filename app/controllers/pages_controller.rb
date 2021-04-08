ffe2d82ab8223be5c598ee0577ccca9764f48643
Raimundo <rjcavada@uc.cl>
2021-04-07 21:50:00 -0400

    Busqueda Lista

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,25 @@
 class PagesController < ApplicationController
     def search
-        @parameter = params[:search].downcase 
-        @response = JSON.parse HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters', :params => {:name => @parameter}).body
+        if params[:info]
+            @parameter = params[:info][:search].downcase
+            @vez = params[:info][:next]
+            @response = JSON.parse HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters', :params => {:name => @parameter, :limit => 10, :offset => @vez}).body
+            @response2 = JSON.parse HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters', :params => {:name => @parameter, :limit => 10, :offset => (@vez.to_i+10)}).body
+            if @response2 == []
+                @vacio = true
+            else
+                @vacio = false
+            end
+        else
+            @parameter = params[:search].downcase 
+            @response = JSON.parse HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters', :params => {:name => @parameter}).body
+            @response2 = JSON.parse HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters', :params => {:name => @parameter, :limit => 10, :offset => 10}).body
+            if @response2 == []
+                @vacio = true
+            else
+                @vacio = false
+            end
+            @vez = 0
+        end
     end
 end
